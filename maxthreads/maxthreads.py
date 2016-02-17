e8ef54aa7488aa55f73d2800a85c48fe40ba4b1a
Zaeb0s <christoffer_zakrisson@hotmail.com>
2016-02-17 17:44:56 +0100

    First upload (seems to work)

---
+++ maxthreads/maxthreads.py
@@ -0,0 +1,33 @@
+#!/bin/env python3
+
+import threading
+
+
+class MaxThreads:
+    def __init__(self, max_threads):
+        self.sema = threading.BoundedSemaphore(max_threads)
+
+    def start_thread(self, target, args=(), kwargs={}):
+        self.sema.acquire()
+        threading.Thread(target=self._target_modifier,
+                         args=(target, ) + args,
+                         kwargs=kwargs).start()
+
+    def _target_modifier(self, target, args=(), kwargs={}):
+        try:
+            target(*args, **kwargs)
+        finally:
+            self.sema.release()
+
+
+
+if __name__ == '__main__':
+    from time import sleep
+    from random import random
+    def w():
+        print(threading.active_count())
+        sleep(random()*2)
+
+    x = MaxThreads(10)
+    for i in range(200):
+        x.start_thread(w)
---
+++ setup.py
@@ -0,0 +1,54 @@
+#!/bin/env python3
+# from distutils.core import setup
+from setuptools import setup
+from maxthreads import __version__
+import sys
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+print('Current version: ', __version__)
+version = __version__.split('.')
+
+
+if sys.argv[-1] == 'minor':
+    version[2] = str(int(version[2]) + 1)
+    del sys.argv[-1]
+elif sys.argv[-1] == 'major':
+    version[1] = str(int(version[1]) + 1)
+    version[2] = '0'
+    del sys.argv[-1]
+elif sys.argv[-1] == 'huge':
+    version[0] = str(int(version[0]) + 1)
+    version[1] = '0'
+    version[2] = '0'
+    del sys.argv[-1]
+
+
+version = '.'.join(version)
+with open('maxthreads/version', 'w') as f:
+    f.write(version)
+
+
+setup(
+    name='maxthreads',
+    packages=['maxthreads'],  # this must be the same as the name above
+    version=version,
+    include_package_data=True,
+    license='MIT',
+    description='Python module for queuing threads',
+    long_description=readme(),
+    author='Christoffer Zakrisson',
+    author_email='christoffer_zakrisson@hotmail.com',
+    url='https://github.com/Zaeb0s/max-threads', # use the URL to the github repo
+    keywords=['max', 'threads', 'thread'], # arbitrary keywords
+    classifiers=['Development Status :: 3 - Alpha',
+                 'Programming Language :: Python :: 3.5',
+                 'Operating System :: POSIX :: Linux',
+                 'License :: OSI Approved :: MIT License'],
+)
+
+
+print('Installed version: ' + version)
