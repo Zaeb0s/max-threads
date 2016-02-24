c5f22a600863ee146c77a8f2ed8803642e867f35
Zaeb0s <christoffer_zakrisson@hotmail.com>
2016-02-24 23:35:53 +0100

    V.0.4.0: Removed ThreadStarter Class because the new way start_thread is written makes this class obsolete (it no longer blocks while waiting for a thread to become available).

--- README.rst
+++ README.rst
@@ -8,3 +8,6 @@ V.0.2.0
 
 V.0.3.0
   - Added ThreadStarter Class. This class can run a separate thread for the sole purpose of starting threads with MaxThreads
+
+V.0.4.0
+  - Removed ThreadStarter Class because the new way start_thread is written makes this class obsolete (it no longer blocks while waiting for a thread to become available).
--- maxthreads/maxthreads.py
+++ maxthreads/maxthreads.py
@@ -47,46 +47,46 @@ class MaxThreads:
             self._threads_active -= 1
 
 
-class ThreadStarter:
-    """This class can run a separate thread for the sole purpose of starting threads with MaxThreads (yeah, I know
-    very specific, but I found myself implementing this or something similar in most programs using MaxThreads)
-    This can be useful if we want to queue up functions to be started in a subthread from a thread that we don't
-    want to be blocked by the MaxThreads object.
-
-    The put funtion puts functions in a queue to be started by MaxThreads.start_thread
-
-    execute_queue
-    """
-    def __init__(self, max_threads=-1):
-        self._ḿaxthreads = MaxThreads(max_threads)
-        self._queue = Queue()
-        self._loop = Loop(target=self._main_loop)
-
-    def serve_forever(self, subthread=True):
-        self._loop.start(subthread=subthread)
-
-    def stop_serve_forever(self):
-        self._loop.stop()
-
-    def _execute_queue(self):
-        while not self._queue.empty():
-            self._main_loop()
-
-    def execute_queue(self, subthread=True):
-        if subthread:
-            threading.Thread(target=self._execute_queue).start()
-        else:
-            self._execute_queue()
-
-    def _main_loop(self):
-        try:
-            target, args, kwargs = self._queue.get(timeout=2)
-        except Empty:
-            pass
-        else:
-            self._ḿaxthreads.start_thread(target=target,
-                                          args=args,
-                                          kwargs=kwargs)
-
-    def put(self, target, args=(), kwargs={}):
-        self._queue.put((target, args, kwargs))
+# class ThreadStarter:
+#     """This class can run a separate thread for the sole purpose of starting threads with MaxThreads (yeah, I know
+#     very specific, but I found myself implementing this or something similar in most programs using MaxThreads)
+#     This can be useful if we want to queue up functions to be started in a subthread from a thread that we don't
+#     want to be blocked by the MaxThreads object.
+#
+#     The put funtion puts functions in a queue to be started by MaxThreads.start_thread
+#
+#     execute_queue
+#     """
+#     def __init__(self, max_threads=-1):
+#         self._ḿaxthreads = MaxThreads(max_threads)
+#         self._queue = Queue()
+#         self._loop = Loop(target=self._main_loop)
+#
+#     def serve_forever(self, subthread=True):
+#         self._loop.start(subthread=subthread)
+#
+#     def stop_serve_forever(self):
+#         self._loop.stop()
+#
+#     def _execute_queue(self):
+#         while not self._queue.empty():
+#             self._main_loop()
+#
+#     def execute_queue(self, subthread=True):
+#         if subthread:
+#             threading.Thread(target=self._execute_queue).start()
+#         else:
+#             self._execute_queue()
+#
+#     def _main_loop(self):
+#         try:
+#             target, args, kwargs = self._queue.get(timeout=2)
+#         except Empty:
+#             pass
+#         else:
+#             self._ḿaxthreads.start_thread(target=target,
+#                                           args=args,
+#                                           kwargs=kwargs)
+#
+#     def put(self, target, args=(), kwargs={}):
+#         self._queue.put((target, args, kwargs))
